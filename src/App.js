0c8d6bf7be4751be7d4482cb71224def4ef5f34a
mr.Seven <mr.upgrade3@protonmail.com>
2023-02-27 20:03:34 +0200

    Update files
    Make refactoring and add solution with useState for control form

--- src/App.js
+++ src/App.js
@@ -1,41 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.scss";
 import { Button } from "react-bootstrap";
 import { Form } from "react-bootstrap";
-
-const dataUsers = [];
+import UserCard from "./components/UserCard";
 
 function App() {
+  const dataUsers = [];
+  const [data, setData] = useState({
+    firstName: "",
+    secondName: "",
+    country: "",
+    city: "",
+    age: "",
+  });
+
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    const collectUser = {
-      firstname: e.target.firstname.value,
-      secondname: e.target.secondname.value,
-      country: e.target.country.value,
-      city: e.target.city.value,
-      age: e.target.age.value,
-    };
-    dataUsers.push(collectUser);
+    dataUsers.push(data);
 
     // Clean inputs
 
-    e.target.firstname.value = "";
-    e.target.secondname.value = "";
-    e.target.country.value = "";
-    e.target.city.value = "";
-    e.target.age.value = "";
-
     console.log(dataUsers);
-    /*
-    console.log({
-      firstname: e.target.firstname.value,
-      secondname: e.target.secondname.value,
-      country: e.target.country.value,
-      city: e.target.city.value,
-      age: e.target.age.value,
-    });
-    */
   };
   return (
     <div className="App">
@@ -48,8 +35,10 @@ function App() {
             <Form.Label className="form__label">First Name</Form.Label>
             <Form.Control
               name="firstname"
+              value={data.firstName}
               type="text"
               placeholder="Enter First Name"
+              onChange={(e) => setData({ ...data, firstName: e.target.value })}
             />
           </Form.Group>
 
@@ -60,8 +49,10 @@ function App() {
             <Form.Label className="form__label">Second Name</Form.Label>
             <Form.Control
               name="secondname"
+              value={data.secondName}
               type="text"
               placeholder="Enter Second Name"
+              onChange={(e) => setData({ ...data, secondName: e.target.value })}
             />
           </Form.Group>
 
@@ -72,8 +63,10 @@ function App() {
             <Form.Label className="form__label">Country</Form.Label>
             <Form.Control
               name="country"
+              value={data.country}
               type="text"
               placeholder="Enter Country"
+              onChange={(e) => setData({ ...data, country: e.target.value })}
             />
           </Form.Group>
 
@@ -82,7 +75,13 @@ function App() {
             controlId="formBasicPassword"
           >
             <Form.Label className="form__label">City</Form.Label>
-            <Form.Control name="city" type="text" placeholder="Enter City" />
+            <Form.Control
+              name="city"
+              value={data.city}
+              type="text"
+              placeholder="Enter City"
+              onChange={(e) => setData({ ...data, city: e.target.value })}
+            />
           </Form.Group>
 
           <Form.Group
@@ -90,13 +89,20 @@ function App() {
             controlId="formBasicPassword"
           >
             <Form.Label className="form__label">Age</Form.Label>
-            <Form.Control name="age" type="number" placeholder="Enter Age" />
+            <Form.Control
+              name="age"
+              value={data.age}
+              type="number"
+              placeholder="Enter Age"
+              onChange={(e) => setData({ ...data, age: e.target.value })}
+            />
           </Form.Group>
 
           <Button className="form__submit" variant="primary" type="submit">
             Submit
           </Button>
         </Form>
+        <UserCard />
       </div>
     </div>
   );
---
+++ src/components/UserCard.js
@@ -0,0 +1,11 @@
+const UserCard = () => {
+  return (
+    <div className="form__user-card">
+      <h2>FirstName SecondName</h2>
+      <h3>Country, City</h3>
+      <p>Age: age</p>
+    </div>
+  );
+};
+
+export default UserCard;
